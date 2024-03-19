1385e1185c1038ab3d6ba3a894937e000649eb47
NICKNAIR <102501152+niyathnair@users.noreply.github.com>
2024-03-19 12:17:58 +0530

    Delete lib directory

--- lib/Screens/screens.dart
+++
@@ -1,5 +0,0 @@
-export 'home_screen.dart';
-export 'playlist_screen.dart';
-export 'song_screen.dart';
-export 'raag_taal_screen.dart';
-export 'taal_det.dart';
--- lib/widgets/widgets.dart
+++
@@ -1,4 +0,0 @@
-export 'section_header.dart';
-export 'song_card.dart';
-export 'playlist_card.dart';
-export 'raag_taal.dart';
